bbe8d710229b1a43269b3cc1a950ef66fc12d0a3
ebradford <emma_bradford@milton.edu>
2024-11-08 08:28:40 -0500

    finished light show

---
+++ LightShow.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  //light show set up!!
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //light show code!
+  //woo!
+    //woo!
+
+  //woo!
+
+  //woo!
+  //ooh look it is the potenti!
+
+  //tri color coolness!
+    //tri color coolness!
+      //tri color coolness!
+        //tri color coolness!
+        //tri color coolness!
+}
